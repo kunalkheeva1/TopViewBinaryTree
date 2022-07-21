a29b4b1805335d3da9e80483381ff8a898c7e40d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 18:14:17 -0700

    added the method to check add the elements in the map to maintian the order and then print it accordingly

--- src/TopViewBinaryTree.java
+++ src/TopViewBinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class TopViewBinaryTree {
 
 
@@ -20,6 +22,46 @@ public class TopViewBinaryTree {
             this.val = val;
             this.node = node;
         }
+
+    }
+
+    public List<Integer> topViewBinaryTree(Node root) {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        ArrayList<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+
+        Queue<Pair> q = new LinkedList<>();
+        Pair pair = new Pair(0, root);
+        q.add(pair);
+
+        while (!q.isEmpty()) {
+
+            Pair pp = q.remove();
+            Node current = pp.node;
+            int value = pp.val;
+
+            if(current.left != null){
+                Pair p1 = new Pair(value -1, current.left);
+                q.add(p1);
+            }
+            if(current.right != null){
+                Pair p2 = new Pair(value +1, current.right);
+                q.add(p2);
+            }
+            if(map.containsKey(value) == false){
+                map.put(value, current.data);
+            }
+
+
+
+
+        }for(Map.Entry<Integer, Integer> entry : map.entrySet())
+        {
+            ans.add(entry.getValue());
+        }
+        return ans;
+
+
 
     }
 
