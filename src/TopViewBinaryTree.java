ae6e82defbd50457af034cde5e68cc00159bd744
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-20 17:58:01 -0700

    Initial commit

---
+++ src/TopViewBinaryTree.java
@@ -0,0 +1,25 @@
+public class TopViewBinaryTree {
+
+
+    class Node{
+        int data;
+        Node right;
+        Node left;
+        Node(int data, Node left, Node right)
+        {
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Pair{
+        int val;
+        
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
